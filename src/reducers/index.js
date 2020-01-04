39befb6715dc99f71526580ef14fb42b5e10a15e
Chris Joyce <chrisj@benon.com>
2020-01-04 14:40:48 +1000

    Baking Browser Cookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = action => ({ text: action.text, dueDate: action.dueDate, id: Math.random() })
 
@@ -9,12 +10,15 @@ const removeById = (state = [], id) => {
 
 const reminders = (state = [], action) => {
     let reminders = null;
+    state = read_cookie('reminders');
     switch (action.type) {
         case ADD_REMINDER:
             reminders = [...state, reminder(action)]
+            bake_cookie('reminders', reminders);
             break;
         case DELETE_REMINDER:
             reminders = removeById(state, action.id);
+            bake_cookie('reminders', reminders);
             break;
         default:
             return state;
